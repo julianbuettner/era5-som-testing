4fd57402d02a48bb468513a1634f9ee4660b6243
Julian Büttner <me@julianbuettner.dev>
2023-12-19 13:50:35 +0100

    Fuck

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import somoclu
 from time import sleep
 from matplotlib.offsetbox import AnchoredText
 import matplotlib.pyplot as plt
@@ -6,98 +5,138 @@ import numpy as np
 import xarray as xr
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-
-COORDS = [-20, 50, 30, 80]
-
-harvest = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
-                    [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
-                    [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
-                    [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
-                    [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
-                    [1.3, 1.2, 0.0, 0.0, 0.0, 3.2, 5.1],
-                    [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3]])
-harvest = np.random.rand(100, 100)
-harvest = np.random.rand(COORDS[1] - COORDS[0], COORDS[3] - COORDS[2])
+from minisom import MiniSom
+from xarray.core.dataarray import DataArray
+from itertools import product
+from random import randint
 
 IMAGE_PATH = "/mnt/c/Users/Julian/Desktop/test.png"
+COORDS = None
+
+DATASET = "full500.nc"
+# DATASET = "test500.nc"
+TRAINING_STEPS = "/mnt/d/som/ani1/training_{:05}.png"
+FIG_FAC = 2
+FIGSIZE = [6.4 * FIG_FAC, 4.8 * FIG_FAC]
+STEPS = 90 * 1000
+BATCHSIZE = 70
 
 def plotting(ax, da: xr.DataArray, index=0):
-    # width = len(data[0])
-    # height = len(data)
-    # x_range = (COORDS[1] - COORDS[0])
-    # y_range =  (COORDS[3] - COORDS[2])
-    # print(x)
-    # print(y)
-    # print(da.values[0])
     x = da.coords["latitude"].values
     y = da.coords["longitude"].values
-    print("Dim:", len(x) * len(y))
-    # print(x, y, da.values[index].shape)
-    # X, Y = np.meshgrid(da.coords["latitude"].values, da.coords["longitude"].values)
     ax.pcolormesh(
-        [yy for yy in y],
-        [xx for xx in x],
-        # da.values[0],
-        da.values[index][:-1, :-1],
-        shading='nearest',
+        y,
+        x,
+        da.values[index],
+        shading="nearest",
     )
 
-def model_plot(model):
-    ...
+
+def model_plot(model, da: xr.DataArray):
+    model = np.array(model)
+    fig = plt.figure(345435, figsize=FIGSIZE)
+    SPACE = 0.1
+    gs = fig.add_gridspec(
+        *model.shape[:2],
+        wspace=SPACE,
+        hspace=SPACE,
+        left=SPACE / 2,
+        right=1 - SPACE / 2,
+        bottom=SPACE / 2,
+        top=1 - SPACE / 2
+    )
+
+    for i, j in product(range(model.shape[0]), range(model.shape[1])):
+        ax = fig.add_subplot(gs[i, j], projection=ccrs.PlateCarree())
+        ax.set_extent(COORDS, crs=ccrs.PlateCarree())
+        ax.stock_img()
+        ax.add_feature(cfeature.LAND)
+        ax.add_feature(cfeature.COASTLINE)
+        ax.coastlines(resolution="50m")
+
+        x = da.coords["latitude"].values
+        y = da.coords["longitude"].values
+
+        x = da.coords["latitude"].values
+        y = da.coords["longitude"].values
+        m = model[i, j].reshape((x.shape[0], y.shape[0]))
+        ax.pcolormesh(
+            y,
+            x,
+            m,
+            vmin=50,
+            vmax=60,
+            shading="nearest",
+            cmap='jet',
+        )
+    return fig
 
 
 def resample(a: xr.Dataset):
     a = a.resample(time="1D").mean()
-    a = a.coarsen(longitude=4, boundary='trim').mean()
-    a = a.coarsen(latitude=4, boundary='trim').mean()
+    a = a.coarsen(longitude=4, boundary="trim").mean()
+    a = a.coarsen(latitude=4, boundary="trim").mean()
+    a = a / 1000
     return a
+
+def load_random_selection_of_data(size=1000):
+    data = xr.open_dataset(DATASET)
+    time_length = len(data["time"])
+    time_start = randint(0, time_length - BATCHSIZE - 1)
+    time_slice = slice(data["time"][time_start], data["time"][time_start + BATCHSIZE])
+    print("Time slice starting from", data["time"][time_start])
+    dataset = data.sel(time=time_slice)
+    return resample(dataset)
 
 
 def main():
-    # plt.style.use('_mpl-gallery-nogrid')
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-    ax.set_extent(COORDS, crs=ccrs.PlateCarree())
+    global COORDS
+    dataset = load_random_selection_of_data()
+    # print(dataset["z"])
+    # return
+    COORDS = [
+        dataset.coords["longitude"][0],
+        dataset.coords["longitude"][-1],
+        dataset.coords["latitude"][0],
+        dataset.coords["latitude"][-1],
+    ]
 
-    # Put a background image on for nice sea rendering.
-    ax.stock_img()
+    # fig = plt.figure(0)
+    # ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    # ax.set_extent(COORDS, crs=ccrs.PlateCarree())
+    # ax.add_feature(cfeature.LAND)
+    # ax.add_feature(cfeature.COASTLINE)
+    # ax.coastlines(resolution="50m")
+    #
+    # sample = geopot.as_numpy().reshape(
+    #     (
+    #         data_dim[0],
+    #         data_dim[1] * data_dim[2],
+    #     )
+    # )
+    model = MiniSom(4, 4, len(dataset.coords["latitude"]) * len(dataset.coords["longitude"]))
+    # model.pca_weights_init(sample[:4])
+    print("Train")
 
-    ax.add_feature(cfeature.LAND)
-    ax.add_feature(cfeature.COASTLINE)
-    ax.coastlines(resolution="50m")
+    for i in range(STEPS):
+        choice = np.random.choice(dataset["time"])
+        sample = dataset["z"].sel(time=choice)
+        sample_np = np.array(sample.to_numpy().flat)
+        model.update(sample_np, model.winner(sample_np), i, STEPS)
+        if i % 1000 == 0 and i > 0:
+            print("Step {}/{}".format(i, STEPS))
+            fig = model_plot(model.get_weights(), dataset)
+            fig.savefig(TRAINING_STEPS.format(i))
+        if i % 1000 == BATCHSIZE / 2:
+            dataset = load_random_selection_of_data()
+    return
 
-    SOURCE = "Natural Earth"
-    LICENSE = "public domain"
-    # Add a text annotation for the license information to the
-    # the bottom right corner.
-    text = AnchoredText(
-        "\u00A9 {}; license: {}" "".format(SOURCE, LICENSE),
-        loc=4,
-        prop={"size": 12},
-        frameon=True,
-    )
-    # ax.add_artist(text)
-
-    data = xr.load_dataset("test500.nc")
-    geopot = data["z"]
-    print(geopot.to_numpy().shape)
-    print("Normalize...")
-    geopot = resample(geopot)
-    data_dim = geopot.to_numpy().shape
-    print("Datapoint dim", data_dim)
-
-    model = somoclu.Somoclu(4, 4)
-    sample = geopot.to_numpy().reshape((data_dim[0], data_dim[1] * data_dim[2],))
-    print("Sample", sample.shape)
-    model.train(sample, epochs=10000)
-    # print("BMUs:", model.get)
-    model.view_umatrix(bestmatches=True, filename=IMAGE_PATH)
-
-    # for i in range(300):
-    #     plotting(ax, geopot, index=i)
-    #     plt.savefig("out.png")
-    #     sleep(1)
-    # plt.savefig("/mnt/c/Users/Julian/Desktop/test.png")
+    plt.figure(0)
+    for i in range(300):
+        plotting(ax, geopot, index=i)
+        plt.savefig(IMAGE_PATH)
+        sleep(1)
+        return
 
 
 if __name__ == "__main__":
